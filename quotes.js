d98439cae4358afb0c6d4fc6903fa526b8bbccd0
TomiPullinen <tpullinen@gmail.com>
2023-01-31 18:20:48 +0200

    quotes working

---
+++ quotes.js
@@ -0,0 +1,14 @@
+const localQuotes = [
+    {
+        text: 'I wipe my a** with your feelings.',
+        author: 'Tony Soprano',
+    },
+    {
+        text: 'That was real? I saw that movie, I thought it was bulls**t.',
+        author: 'Christopher Moltisanti',
+    },
+    {
+        text: 'From now on everyday is a gift',
+        author: 'Tony Soprano'
+    }
+]
---
+++ script.js
@@ -0,0 +1,38 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // Check if Author field is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+
+    quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
+// async function getQuotes() {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
+// On Load
+//getQuotes();
+newQuote();
